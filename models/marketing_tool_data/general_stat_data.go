1878c2869af3397976f40132fbb919e3860cdaae
zhangjinzhu <zhangjinzhu@meicai.cn>
2020-12-23 12:18:59 +0800

    examples

--- models/marketing_tool_data/general_stat_data.go
+++ models/marketing_tool_data/general_stat_data.go
@@ -11,7 +11,7 @@ import (
 
 // GeneralStatData 实体
 type GeneralStatData struct {
-	common.BaseModelV2
+	common.BaseModelV1
 	MainId uint `gorm:"column:main_id;" json:"main_id" form:"main_id" binding:"required"`
 	//WxSystemUserId              int    `gorm:"column:wx_system_user_id;" json:"wx_system_user_id" form:"wx_system_user_id" binding:"required"`
 	//ToolId                      int    `gorm:"column:tool_id;" json:"tool_id" form:"tool_id" binding:"required"`
@@ -20,31 +20,33 @@ type GeneralStatData struct {
 	ToolId                 uint   `gorm:"column:tool_id;" json:"tool_id" form:"tool_id"`
 	ToolType               int8   `gorm:"column:tool_type;" json:"tool_type" form:"tool_type"`
 	DateTime               string `gorm:"column:date_time;" json:"date_time" form:"date_time"`
-	VisitTotalTimes        uint   `gorm:"column:visit_total_times;" json:"visit_total_times" form:"visit_total_times"`
-	VisitTotalNum          uint   `gorm:"column:visit_total_num;" json:"visit_total_num" form:"visit_total_num"`
+	VisitTimes             uint   `gorm:"column:visit_times;" json:"visit_times" form:"visit_times"`
+	VisitNum               uint   `gorm:"column:visit_num;" json:"visit_num" form:"visit_num"`
 	EnrollNum              uint   `gorm:"column:enroll_num;" json:"enroll_num" form:"enroll_num"`
 	EnrollApprovePassedNum uint   `gorm:"column:enroll_approve_passed_num;" json:"enroll_approve_passed_num" form:"enroll_approve_passed_num"`
 	ClickEnterLiveTimes    uint   `gorm:"column:click_enter_live_times;" json:"click_enter_live_times" form:"click_enter_live_times"`
 	ClickEnterLiveNum      uint   `gorm:"column:click_enter_live_num;" json:"click_enter_live_num" form:"click_enter_live_num"`
 	ClickWatchReplayTimes  uint   `gorm:"column:click_watch_replay_times;" json:"click_watch_replay_times" form:"click_watch_replay_times"`
 	ClickWatchReplayNum    uint   `gorm:"column:click_watch_replay_num;" json:"click_watch_replay_num" form:"click_watch_replay_num"`
-	DownloadTotalTimes     uint   `gorm:"column:download_total_times;" json:"download_total_times" form:"download_total_times"`
+	DownloadTimes          uint   `gorm:"column:download_times;" json:"download_times" form:"download_times"`
 	DownloadNum            uint   `gorm:"column:download_num;" json:"download_num" form:"download_num"`
-	ShareTotalTimes        uint   `gorm:"column:share_total_times;" json:"share_total_times" form:"share_total_times"`
+	ShareTimes             uint   `gorm:"column:share_times;" json:"share_times" form:"share_times"`
 	ShowNum                uint   `gorm:"column:show_num;" json:"show_num" form:"show_num"`
-	FavoriteTotalTimes     uint   `gorm:"column:favorite_total_times;" json:"favorite_total_times" form:"favorite_total_times"`
+	FavoriteTimes          uint   `gorm:"column:favorite_times;" json:"favorite_times" form:"favorite_times"`
 	FavoriteNum            uint   `gorm:"column:favorite_num;" json:"favorite_num" form:"favorite_num"`
-	ViewMaterialTotalTimes uint   `gorm:"column:view_material_total_times;" json:"view_material_total_times" form:"view_material_total_times"`
+	ViewMaterialTimes      uint   `gorm:"column:view_material_times;" json:"view_material_times" form:"view_material_times"`
 	ViewMaterialNum        uint   `gorm:"column:view_material_num;" json:"view_material_num" form:"view_material_num"`
-	NewFansNum             uint   `gorm:"column:new_fans_num;" json:"new_fans_num" form:"new_fans_num"`
-	NewMemberNum           uint   `gorm:"column:new_member_num;" json:"new_member_num" form:"new_member_num"`
-	SignNum                uint   `gorm:"column:sign_num;" json:"sign_num" form:"sign_num"`
-	JoinNum                uint   `gorm:"column:join_num;" json:"join_num" form:"join_num"`
-	JoinRate               uint   `gorm:"column:join_rate;" json:"join_rate" form:"join_rate"`
-	TransferToLeadsNum     uint   `gorm:"column:transfer_to_leads_num;" json:"transfer_to_leads_num" form:"transfer_to_leads_num"`
-	TransferToLeadsRate    uint   `gorm:"column:transfer_to_leads_rate;" json:"transfer_to_leads_rate" form:"transfer_to_leads_rate"`
-	AcceptAsLeadsNum       uint   `gorm:"column:accept_as_leads_num;" json:"accept_as_leads_num" form:"accept_as_leads_num"`
-	AcceptAsLeadsRate      uint   `gorm:"column:accept_as_leads_rate;" json:"accept_as_leads_rate" form:"accept_as_leads_rate"`
+	/*
+		NewFansNum             uint   `gorm:"column:new_fans_num;" json:"new_fans_num" form:"new_fans_num"`
+		NewMemberNum           uint   `gorm:"column:new_member_num;" json:"new_member_num" form:"new_member_num"`
+		SignNum                uint   `gorm:"column:sign_num;" json:"sign_num" form:"sign_num"`
+		JoinNum                uint   `gorm:"column:join_num;" json:"join_num" form:"join_num"`
+		JoinRate               uint   `gorm:"column:join_rate;" json:"join_rate" form:"join_rate"`
+		TransferToLeadsNum     uint   `gorm:"column:transfer_to_leads_num;" json:"transfer_to_leads_num" form:"transfer_to_leads_num"`
+		TransferToLeadsRate    uint   `gorm:"column:transfer_to_leads_rate;" json:"transfer_to_leads_rate" form:"transfer_to_leads_rate"`
+		AcceptAsLeadsNum       uint   `gorm:"column:accept_as_leads_num;" json:"accept_as_leads_num" form:"accept_as_leads_num"`
+		AcceptAsLeadsRate      uint   `gorm:"column:accept_as_leads_rate;" json:"accept_as_leads_rate" form:"accept_as_leads_rate"`
+	*/
 }
 
 /*
